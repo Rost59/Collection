237b056d8debfcd38c4378f7f319a50e444234d0
Rost87 <rost_01_87@mail.ru>
2021-01-21 19:48:43 +0500

    Добавлен комментарий. Для файла выбрана другая ветка: branchFirTest

--- src/com/company/Colors.java
+++ src/com/company/Colors.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Colors {
+    // Комментарий для новой ветки branchForTest
+
     // и еще комментарий
     // Еще комментарий
     // ДОБАВЛЯЮ КОММЕНТАРИЙ
