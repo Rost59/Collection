074ac4d256030795ee258df9049f1fc91c827f51
Rost87 <rost_01_87@mail.ru>
2021-01-27 17:58:14 +0500

    Добавил комментарий1 основной ветке Master

--- src/com/company/TestFork.java
+++ src/com/company/TestFork.java
@@ -3,5 +3,6 @@ package com.company;
 public class TestFork {
     public static void main(String[] args) {
         System.out.println("Начало работы. Работа в основной ветке Master");
+        //комментарий 1
     }
 }
