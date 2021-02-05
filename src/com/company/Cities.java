1efa7842a95eada9ee4efdad0986257495fd45d6
Rost87 <34788968+Rost87@users.noreply.github.com>
2021-02-05 18:06:15 +0500

    Добавлен HashMap: ключ - первая буква города, значение - массив городов, начинающихся на эту букву

--- src/com/company/Cities.java
+++ src/com/company/Cities.java
@@ -1,14 +1,12 @@
 package com.company;
 
 import java.io.File;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class Cities {
 
 
-    static boolean checkCityLetter(String needCity, String city) {
+    static char checkCityLetter(String needCity) {
 
         char cityChar = needCity.charAt(needCity.length() - 1);
 
@@ -31,37 +29,58 @@ public class Cities {
                 break;
         }
 
-        return cityChar == city.charAt(0);
+        return cityChar;
     }
 
     // поиск города компьютером
-    public static String computerStep(HashSet allCities, HashSet namedCities, String needCity){
-        String city = "";
+    public static String computerStep(Map<Character, ArrayList<String>> firstLetterCities, Set<String> namedCities, String needCity){
+        String city = "Игра закончена. Города, с названиями начинающмися на данную букву, отсутствуют.";
+        // буква на которую должен начинаться искомый город
+        char needLetter = checkCityLetter(needCity);
+        // список всех городов, начинающихся на искомую букву
+        ArrayList<String> cities = firstLetterCities.get(needLetter);
 
-        Iterator<String> i = allCities.iterator();
-        while (i.hasNext()) {
-            city = i.next().toLowerCase();
-            if (checkCityLetter(needCity, city) && !namedCities.contains(city)) {
-                System.out.println(city);
-                return city;
+        if (cities == null){
+            return city;
+        }
+        for (String s : cities) {
+            if (!namedCities.contains(s)) {
+                city = s;
+                // namedCities.add(city); если раскомментировать эту строку, то namedCities, объявленный в main, будет меняться
+                break;
             }
         }
-        return city = "Города с названиями, начинающмися на данную букву, отсутствуют";
+
+        return city;
     }
 
-    public static String personStep(HashSet allCities, HashSet namedCities, String needCity){
+    public static String personStep(Map<Character, ArrayList<String>> firstLetterCities, Set<String> namedCities, String needCity, String personName){
         String city = "";
         Scanner scan = new Scanner(System.in);
         boolean notRightCity = true;
+        // буква на которую должен начинаться искомый город
+        char needLetter = checkCityLetter(needCity);
+        // список всех городов, начинающихся на искомую букву
+        ArrayList<String> cities = firstLetterCities.get(needLetter);
+
+        if (cities == null){
+            city = "Игра закончена. Города, с названиями начинающмися на данную букву, отсутствуют.";
+            return city;
+        }
         do {
-            System.out.println("Введите название города (или выход для окончания игры): ");
-            city = scan.next().toLowerCase();
+            System.out.println(personName + " введите название города (или выход для окончания игры): ");
+            city = scan.nextLine().toLowerCase();
 
             if(city.equals("выход")){
                 break;
             }
 
-            if (!allCities.contains(city)) {
+            if (needLetter != city.charAt(0)) {
+                System.out.println("Нужно написать город, начинающийся на букву " + needLetter);
+                continue;
+            }
+
+            if (!cities.contains(city)) {
                 System.out.println("Такой город не существует");
                 continue;
             }
@@ -70,8 +89,8 @@ public class Cities {
                 System.out.println("Такой город уже был назван ранее");
                 continue;
             }
+            notRightCity = false;
 
-            notRightCity = !checkCityLetter(needCity, city);
         } while (notRightCity);
         return city;
     }
@@ -79,119 +98,96 @@ public class Cities {
 
 
 
-    // mode (режим игры): 1 - с компьютером, 2 - с игроком
-    static String findingCity(HashSet allCities, HashSet namedCities, String needCity, int step) {
-        String city = "";
-        Scanner scan = new Scanner(System.in);
-        boolean notRightCity = true;
-        if (step == 1) {
-            Iterator<String> i = allCities.iterator();
-            while (i.hasNext()) {
-                city = i.next().toLowerCase();
-                if (checkCityLetter(needCity, city) && !namedCities.contains(city)) {
-                    System.out.println(city);
-                    return city;
-                }
-            }
-            return city = "Города с названиями, начинающмися на данную букву, отсутствуют";
-        } else {
-            do {
-                System.out.println("Введите название города (или выход для окончания игры): ");
-                city = scan.next().toLowerCase();
-
-                if(city.equals("выход")){
-                    break;
-                }
-
-                if (!allCities.contains(city)) {
-                    System.out.println("Такой город не существует");
-                    continue;
-                }
-
-                if (namedCities.contains(city)) {
-                    System.out.println("Такой город уже был назван ранее");
-                    continue;
-                }
-
-                notRightCity = !checkCityLetter(needCity, city);
-            } while (notRightCity);
-            return city;
-
-        }
-
-    }
-
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         // список всех городов
-        HashSet<String> allCities = new HashSet<>();
+        Set<String> allCities = new TreeSet<>();
         // для хранения уже использованных названий городов
         HashSet<String> namedCities = new HashSet<>();
-        // 0 - ход игрока, 1 - ход компьютера
         int step = 0;
-        String city = "Москва";
+        int mode = 1;
+        String city = "";
         String firstPersonName = "", secondPersonName = "";
-
 
         System.out.println("Выберите режим игры: ");
         System.out.println("Цифра 1 - игра с компьютером, цифра 2 - игра с другим игроком");
-        int mode = myScanner.nextInt();
 
+        try {
+            mode = myScanner.nextInt();
+        } catch (Exception exc){
+            System.out.println("Вы ввели число отличное от 1 и 2. По умолчанию выбран режим игры с компьютером");
+            myScanner.nextLine();
+        }
         // для любого режима игры считываю названия городов из файла и записываю в HashSet
         try (Scanner scanner = new Scanner(new File("cities.txt"));) {
             while (scanner.hasNext()) {
-                allCities.add(scanner.next().toLowerCase());
+                allCities.add(scanner.nextLine().toLowerCase());
             }
 
         } catch (Exception exc) {
-
+            System.out.println(exc);
         }
 
-        if (mode == 1) {
-            System.out.println("Введите ваше имя: ");
-            firstPersonName = myScanner.next();
-            System.out.println(firstPersonName + " введите название города (или выход для окончания игры): ");
-
-        } else {
-            System.out.println("Первый игрок введите ваше имя: ");
-            firstPersonName = myScanner.next();
-            System.out.println("Второй игрок введите ваше имя: ");
-            secondPersonName = myScanner.next();
-            System.out.println(firstPersonName + " введите название города (или выход для окончания игры): ");
-            city = myScanner.next().toLowerCase();
-            while(allCities.contains(city)){
-                System.out.println("Такой город не существует");
-
+        char letter = 'а';
+        ArrayList<String> equalLetterCities = new ArrayList<>();
+        Map<Character, ArrayList<String>> firstLetterCities= new TreeMap<>();
+        // создания словаря: ключ - первая буква в названии города, значение - массив городов, начинающихся на эту букву
+        for (String allCity : allCities) {
+            city = allCity.toLowerCase();
+            if (city.charAt(0) != letter) {
+                firstLetterCities.put(letter, (ArrayList<String>) equalLetterCities.clone());
+                letter = city.charAt(0);
+                equalLetterCities.clear();
             }
+            equalLetterCities.add(city);
 
-            do {
-               if (!allCities.contains(city)) {
-                   System.out.println("Такой город не существует");
-               } else break;
-            } while()
-
-            namedCities.add(city);
         }
+
+        firstLetterCities.put(letter, (ArrayList<String>) equalLetterCities.clone());
+
+        System.out.println("Первый игрок введите ваше имя: ");
+        myScanner.nextLine();
+        firstPersonName = myScanner.nextLine();
+
+        // если mode = 2, то выбран режим игры со вторым игроком
+        if(mode == 2){
+            System.out.println("Второй игрок введите ваше имя: ");
+            secondPersonName = myScanner.nextLine();
+        }
+
+        System.out.println(firstPersonName + " введите название города (или выход для окончания игры): ");
+        city = myScanner.nextLine().toLowerCase();
+
+        while(!allCities.contains(city)){
+            System.out.println("Такой город не существует");
+            System.out.println(firstPersonName + " введите название города (или выход для окончания игры): ");
+            city = myScanner.nextLine().toLowerCase();
+        }
+
+        namedCities.add(city);
 
         do {
-
             // если mode = 1, то выбран режим игры с компьютером
             if (mode == 1) {
-                // step % 2 == 0 - ход игрока, step % 2 != 0 - ход компьютера
+                // step % 2 == 0 - ход игрока, иначе - ход компьютера
                 if (step % 2 == 0){
-                    personStep(allCities, namedCities, city);
+                    city = computerStep(firstLetterCities, namedCities, city);
+                    System.out.println(city);
                 } else{
-                    computerStep(allCities, namedCities, city);
+                    city = personStep(firstLetterCities, namedCities, city, firstPersonName);
                 }
-                step++;
+
             } else{
+                if (step % 2 == 0) {
+                    city = personStep(firstLetterCities, namedCities, city, secondPersonName);
 
+                } else{
+                    city = personStep(firstLetterCities, namedCities, city, firstPersonName);
+                }
             }
-
-
-
+            step++;
             namedCities.add(city.toLowerCase());
-        } while (!city.toLowerCase().equals("выход"));
+        } while (!city.toLowerCase().equals("выход") && (!city.contains("Игра закончена")));
 
 
     }
